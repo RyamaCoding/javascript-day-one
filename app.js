e25101eac1dce38da86cbd9c1afdf05ad0277fd7
Raghu Rayamajhi <ryamacoding@outlook.com>
2022-10-02 13:34:41 +0100

    learning functions

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-for (let i = 1; i <= 20; ++i) {
-    if (i % 3 === 0 && i % 5 ===0) {
-        console.log(`${i} -> Frontend Simplified`)
-    }
+console.log("Welcome to FES, David")
+console.log("Welcome to FES, Zen")
+console.log("Welcome to FES, Mitri")
 
-    else if (i % 3 === 0){
-        console.log(`${i} -> Frontend`)
-    }
 
-    else if (i % 5 === 0){
-        console.log(`${i} -> Simplified`)
-    }
-
-    else {
-        console.log(`${i} -> ${i}`)
-    }
+// Function definition
+function welcomePersonToFes () {
+    console.log("Welcome to FES, David")
 }
+
+
+// Call the function
+welcomePersonToFes();
