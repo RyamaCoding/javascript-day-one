0bdc88110145f592a5de762860c79b7ea5447ed1
Raghu Rayamajhi <ryamacoding@outlook.com>
2022-10-03 12:52:00 +0100

    login and password details

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
-let dollars = [1, 5, 10, 3]
+let userFirstName = 'David'
+let userLastName = 'Bragg'
+let userDiscordId = 'Dbraggles#0001'
+let userSubscriptionStatus = 'VIP'
 
-let cents = []
+let user = [{
+    username: 'David',
+    email: 'david@fes.com',
+    password: 'test123',
+    subscriptionStatus: 'VIP',
+    discordId: 'Dbraggles#0001',
+    lessonsCompleted: [0, 1],
+}, 
+{
+    username: 'Mitri',
+    email: 'mitri@fes.com',
+    password: 'mitri123',
+    subscriptionStatus: 'VIP',
+    discordId: 'Mitri#0001',
+    lessonsCompleted: [0, 1, 2, 3],
+}, 
+{
+    username: 'Zen',
+    email: 'zen@fes.com',
+    password: 'zen123',
+    subscriptionStatus: 'VIP',
+    discordId: 'zen#0001',
+    lessonsCompleted: [0, 1, 2, 3, 4, 5, 6, 7],
+}
+]
 
-for (i = 0; i < dollars.length; i++) {
-    cents.push(dollars[i] * 100)
+function login(email, password) {
+    for (let i = 0; i < user.length; i++) {   
+        if (user[i].email === email) {
+            console.log(user[i]);
+            if (user[i].password === password){
+                console.log('log the user in - the details are correct')
+            }
+            else {
+                console.log('password is incorrect - try again')
+            }
+        }
+    }
 }
 
-console.log(cents)
+login('david@fes.com', 'test123')
+
+
+
