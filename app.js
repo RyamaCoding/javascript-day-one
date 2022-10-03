d9ed16c7b05db82fb7a24958a70ab04cfee70cef
Raghu Rayamajhi <ryamacoding@outlook.com>
2022-10-03 13:42:03 +0100

    register function

--- app.js
+++ app.js
@@ -1,49 +1,58 @@
-let userFirstName = 'David'
-let userLastName = 'Bragg'
-let userDiscordId = 'Dbraggles#0001'
-let userSubscriptionStatus = 'VIP'
-
-let user = [{
-    username: 'David',
-    email: 'david@fes.com',
-    password: 'test123',
-    subscriptionStatus: 'VIP',
-    discordId: 'Dbraggles#0001',
+let users = [
+  {
+    username: "David",
+    email: "david@fes.com",
+    password: "test123",
+    subscriptionStatus: "VIP",
+    discordId: "Dbraggles#0001",
     lessonsCompleted: [0, 1],
-}, 
-{
-    username: 'Mitri',
-    email: 'mitri@fes.com',
-    password: 'mitri123',
-    subscriptionStatus: 'VIP',
-    discordId: 'Mitri#0001',
+  },
+  {
+    username: "Mitri",
+    email: "mitri@fes.com",
+    password: "mitri123",
+    subscriptionStatus: "VIP",
+    discordId: "Mitri#0001",
     lessonsCompleted: [0, 1, 2, 3],
-}, 
-{
-    username: 'Zen',
-    email: 'zen@fes.com',
-    password: 'zen123',
-    subscriptionStatus: 'VIP',
-    discordId: 'zen#0001',
+  },
+  {
+    username: "Zen",
+    email: "zen@fes.com",
+    password: "zen123",
+    subscriptionStatus: "VIP",
+    discordId: "zen#0001",
     lessonsCompleted: [0, 1, 2, 3, 4, 5, 6, 7],
+  },
+];
+
+function register(user) {
+  users.push(user);
 }
-]
+
+register({
+  username: "David",
+  email: "david@fes.com",
+  password: "test124",
+  subscriptionStatus: "VIP",
+  discordId: "Dbraggles#0002",
+  lessonsCompleted: [0, 1],
+});
 
 function login(email, password) {
-    for (let i = 0; i < user.length; i++) {   
-        if (user[i].email === email) {
-            console.log(user[i]);
-            if (user[i].password === password){
-                console.log('log the user in - the details are correct')
-            }
-            else {
-                console.log('password is incorrect - try again')
-            }
-        }
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email) {
+      console.log(users[i]);
+      if (users[i].password === password) {
+        console.log("log the user in - the details are correct");
+      } else {
+        console.log("password is incorrect - try again");
+      }
+      return;
     }
+  }
+  console.log("could not find an email that matches");
 }
 
 login('david@fes.com', 'test123')
 
-
-
+console.log(users);
