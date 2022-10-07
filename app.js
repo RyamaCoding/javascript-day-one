84ca92cdabc5615d89b0165d3c031d384d4afe89
Raghu Rayamajhi <ryamacoding@outlook.com>
2022-10-07 14:32:43 +0100

    learning promises

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-function toggleDarkMode() {
-  document.querySelector('body').classList.toggle("dark-theme")
+const emailRef = document.querySelector(".email")
+
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
 }
+
+main() ;
