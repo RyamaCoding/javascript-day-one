2a77480eaeb2b83685745f3c8818ca4e625261f5
Raghu Rayamajhi <ryamacoding@outlook.com>
2022-10-10 17:12:32 +0100

    creating new promises and using try catch feature to show errors

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const emailRef = document.querySelector(".email")
+const statusRef = document.querySelector(".status")
+const videoRef = document.querySelector(".video")
 
-async function main() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    const data = await response.json()
-    console.log(data)
-    emailRef.innerHTML = data.email
+function getSubscriptionStatus() {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve("VIP")
+        }, 2000)
+    })
 }
 
-main() ;
+function getVideo (subscriptionStatus) {
+    return new Promise ((resolve, reject) => {
+        if (subscriptionStatus === "VIP") {
+            resolve("show video")
+        }
+
+        else if (subscriptionStatus === "FREE") {
+            resolve("show trailer")
+        }
+        else {
+            reject("no video")
+        }
+    }) 
+}
+
+async function main() {
+    const status = await getSubscriptionStatus();
+    statusRef.innerHTML = status;
+    try {
+        console.log(await getVideo(status))
+    }
+    catch (e) {
+        console.log(e)
+        videoRef.innerHTML = e;
+    }
+}
+
+main ();
