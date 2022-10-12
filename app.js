43d45ff325485f157087cf510b3394bec70e3497
Raghu Rayamajhi <ryamacoding@outlook.com>
2022-10-12 15:24:21 +0100

    revisiting crash course and creating changing text

--- app.js
+++ app.js
@@ -1,39 +1,5 @@
-const statusRef = document.querySelector(".status")
-const videoRef = document.querySelector(".video")
+document.querySelector(".title").innerHTML += 'Frontend Simplified'
 
-function getSubscriptionStatus() {
-    return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-            resolve("VIP")
-        }, 2000)
-    })
+function changeTitleToRed() {
+    document.querySelector(".title").classList.toggle("red-theme")
 }
-
-function getVideo (subscriptionStatus) {
-    return new Promise ((resolve, reject) => {
-        if (subscriptionStatus === "VIP") {
-            resolve("show video")
-        }
-
-        else if (subscriptionStatus === "FREE") {
-            resolve("show trailer")
-        }
-        else {
-            reject("no video")
-        }
-    }) 
-}
-
-async function main() {
-    const status = await getSubscriptionStatus();
-    statusRef.innerHTML = status;
-    try {
-        console.log(await getVideo(status))
-    }
-    catch (e) {
-        console.log(e)
-        videoRef.innerHTML = e;
-    }
-}
-
-main ();
